142d3fd73a4aac0429a25eccc19536e31543be28
Peter Karman <perl@peknet.com>
2007-10-17 18:38:48 +0000

    Import of KARMAN/SVN-Class-0.04 from CPAN.
    gitpan-cpan-distribution: SVN-Class
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KARMAN/SVN-Class-0.04.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- lib/SVN/Class/Dir.pm
+++ lib/SVN/Class/Dir.pm
@@ -4,7 +4,7 @@ use warnings;
 use base qw( Path::Class::Dir SVN::Class );
 use SVN::Class::File;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 # override some Path::Class stuff to return SVN::Class objects instead
 sub file {
